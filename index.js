cbc984630883c2d536b83c6ef367e1176ca80d9e
crazy0codes <madhanpanja@gmail.com>
2022-10-04 16:44:44 +0530

    change

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-function change() {
-    console.log("Madhan");
-}
+function add() {
+    let x = document.getElementById("btn-add");
+    if (x.className === "wallet-add-icon") {
+      x.className += " disc";
+      document.getElementById('modal').style.display = "block"
+    } else {
+      x.className = "wallet-add-icon";
+      document.getElementById('modal').style.display = "none"
+    }
+  }
